7db25c0b78455f07ffd027ef9a05315f85c037df
phmr2000 <phmr2000@gmail.com>
2025-09-15 19:35:14 -0300

    Adiciona arquivos de exercício

---
+++ exercicio_guiado/somar_numeros.py
@@ -0,0 +1,12 @@
+def somar(lista): #função que soma uma lista de números
+    return sum (lista)
+a = int(input("Digite o primeiro nùmero:"))
+b = int(input("Digite o segundo nùmero:"))
+c = int(input("Digite o terceiro nùmero:"))
+{"lista": [a,b,c]}
+print("Soma:", somar([a,b,c]))
+
+"""def somar_lista(numeros): #função que soma uma lista de números
+    return sum (numeros)
+{"numeros": [2,4,5,7,8,9]}
+print("Soma:", somar_lista([2,4,5,7,8,9]))"""
