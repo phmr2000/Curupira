20cbbb13180844d503aea7f7f80469994cc8583c
Pedro Henrique <phmr2000@gmail.com>
2025-09-15 20:45:55 -0300

    Update 01.py

--- 01.py
+++ 01.py
@@ -4,11 +4,12 @@ class Curupira:
         self.poder = poder
         self.maturidade = maturidade
 
-    def mostrar_informações(self):
+    def mostrar_informacoes(self): 
         return f"Curupira - Altura: {self.altura}m | Poder: {self.poder} | Maturidade: {self.maturidade}"
 
-    curupira1 = Curupira(1.40, "Proteger a floresta", "Jovem")
-    curupira2 = Curupira(1.60, "Confundir caçadores", "Adulto")
 
-    print(curupira1.mostrar_informacoes())
-    print(curupira2.mostrar_informacoes())
+curupira1 = Curupira(1.40, "Proteger a floresta", "Jovem")
+curupira2 = Curupira(1.60, "Confundir caçadores", "Adulto")
+
+print(curupira1.mostrar_informacoes())
+print(curupira2.mostrar_informacoes())
